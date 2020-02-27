93d7bd11e2b4aea9437966f0457f4f1b24a6ad24
Mohammed Nayeem <mohammednayeem45@gmail.com>
2020-02-27 11:01:46 +0530

    snprintf.c: switch b/w snprintf & sprintf using preprocessor directives.

--- README.md
+++ README.md
@@ -11,3 +11,14 @@ https://www.geeksforgeeks.org/function-pointer-in-c/
 Dynamic loader:
 gcc -shared -fPIC dyn_loading.c -o dloader.so
 gcc -g dyn_loader.c -ldl
+
+snprintf:
+
+gcc snprintf.c -DSNPRINTF
+	snprintf.c:3:0: warning: "SNPRINTF" redefined
+ 	#define SNPRINTF
+--> Hence used #ifndef
+
+gcc snprintf.c -DSPRINTF
+
+
--- snprintf.c
+++ snprintf.c
@@ -1,9 +1,20 @@
 #include<stdio.h>
 
+#ifndef SNPRINTF 
+#define SNPRINTF
+#endif
+
 int main(){
 //char *buf;
 char buf[512];
-snprintf(buf,512,"%d%s%f%s",10,"_",(float)1.56,"_");
+
+#ifdef SNPRINTF
+snprintf(buf,512,"%d%s%f%s%s",10,"_",(float)1.56,"_","sn_printf");
+#endif
+
+#ifdef SPRINTF
+sprintf(buf,"%d%s%f%s%s",10,"_",(float)1.56,"_","sprintf");
+#endif
 printf("buf=%s\n",buf);
 return 0;
 }
