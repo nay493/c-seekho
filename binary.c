4d7f55b7819ac11f4e8312b9be42da864ef377d7
Mohammed Nayeem <mohammednayeem45@gmail.com>
2020-02-28 12:32:07 +0530

    binary.c: Added decimal to binary conversion logic

--- binary.c
+++ binary.c
@@ -1,13 +1,50 @@
+/*
+C program to find Binary Addition and Binary Subtraction.
+
+Author: Mohammed Nayeem.
+
+*/
+
 #include<stdio.h>
 #include<math.h>
 
-#define PF(x) printf("In file:%s, func:%s, line:%d, #x=%lf\n",__FILE__,__func__,__LINE__,x)
+#define PF_d(x) printf("In file:%s, func:%s, line:%d, %s=%lf\n",__FILE__,__func__,__LINE__,#x,x)
+#define PF_i(x) printf("In file:%s, func:%s, line:%d, %s=%d\n",__FILE__,__func__,__LINE__,#x,x)
+#define PF_lu(x) printf("In file:%s, func:%s, line:%d, %s=%lu\n",__FILE__,__func__,__LINE__,#x,x)
 
+//Declarations
 int scratch(void);
+int decimal_to_binary(int);
+int binary_to_decimal(int);
 
 int scratch(){
-  PF(pow(2,3));
+  PF_d(pow(2,3));
+
+  int y = 0b1111; //15
+  int z = 1111;
+  PF_i(y);
+  PF_i(123);
+  PF_lu(sizeof(int));
+
+  // compile to find macro expansion
+  //how to find type of any data type
+  //printf("%d",typeof(x));
   return 0;
+}
+
+
+int decimal_to_binary(int d){
+int b=0,i;
+for(i=31;i>=0;i--){
+  if((i+1)%4 == 0)
+    printf(" ");
+  if(d&(1<<i))
+    printf("1");
+  else
+    printf("0");
+}
+printf("\n");
+return b;
 }
 
 int main() {
@@ -16,6 +53,7 @@ int main() {
   scratch();
   #endif
 
+  decimal_to_binary(4);
 
   return 0;
 }
